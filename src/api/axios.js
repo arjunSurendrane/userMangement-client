e1630787afa3c42a83c6bca69769df33f997aecb
Arjun E <arjunrk907@gmail.com>
2023-06-02 15:36:41 +0530

    add new api link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:3001/api/v1";
+export const url = "https://mavens-api.onrender.com/api/v1";
 
 const instance = axios.create({
   baseURL: url,
